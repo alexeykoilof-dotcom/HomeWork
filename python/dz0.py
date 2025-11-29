5ec46b10e50ffe51c6350e05e9eccdc891f4d0c1
luvshade <alexeykoilof@gmail.com>
2025-11-29 18:33:08 +0800

    Add files via upload

---
+++ python/dz0.py
@@ -0,0 +1,22 @@
+# Задание 1
+a = int(input())
+b = int(input())
+c = int(input())
+print(a + b + c)
+print(a * b * c)
+
+# Задание 2
+zp = int(input())
+kredit = int(input())
+kommunalka = int(input())
+print(zp - kredit - kommunalka)
+
+# Задание 3
+d1 = int(input())
+d2 = int(input())
+print((d1 * d2) / 2)
+
+# Задание 4
+print("To be")
+print("or not")
+print("to be")
